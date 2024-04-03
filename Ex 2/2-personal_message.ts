e71f06f556af95bb3a646626821a40c706703ba8
nadeem-ahmed-77 <165781212+nadeem-ahmed-77@users.noreply.github.com>
2024-04-04 03:19:51 +0500

    Add files via upload

---
+++ Ex 2/2-personal_message.ts
@@ -0,0 +1,4 @@
+// 2. Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+let mess = "Eric";
+console.log("Hello " + mess + ", would you like to learn some Python today?");
