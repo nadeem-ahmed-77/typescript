aec95777603a61749cc6efb739b1749c264d1941
nadeem-ahmed-77 <165781212+nadeem-ahmed-77@users.noreply.github.com>
2024-04-11 20:27:47 +0500

    Add files via upload

---
+++ Ex10/10-adding_comment.ts
@@ -0,0 +1,17 @@
+// 10. Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write
+// because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence
+// describing what the program does.
+
+
+
+// program created by Nadeem Ahmed 
+// dated: 20-02-2024
+
+// this program will print my name. 
+
+console.log("Nadeem Ahmed");
+
+
+// here is another program which add some numbers
+
+console.log(6+2);
