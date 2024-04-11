a207d90b772abb4d637d5bd6a046f702d2c016e4
nadeem-ahmed-77 <165781212+nadeem-ahmed-77@users.noreply.github.com>
2024-04-11 20:40:37 +0500

    Add files via upload

---
+++ Ex14/14-invitation.js
@@ -0,0 +1,6 @@
+"use strict";
+// 14. Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+let invitation = ['Zubair', 'Wahaj', 'Haider'];
+for (let i = 0; i < invitation.length; i++) {
+    console.log('Dear Mr. ' + invitation[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThank You!');
+}
