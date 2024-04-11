85bc924d6eeafb8266ba012374dc4a8570c6da72
nadeem-ahmed-77 <165781212+nadeem-ahmed-77@users.noreply.github.com>
2024-04-11 20:57:54 +0500

    Add files via upload

---
+++ Ex20/20-store_array.ts
@@ -0,0 +1,22 @@
+// 20. Think of something you could store in a array. For example,
+//  you could make a list of mountains, rivers, countries, cities,
+//  languages, or anything else you’d like. Write a program that 
+// creates a list containing these items.
+
+let arrayCities: string [] = ['Karachi', 'Islamabad', 'Multan', 'Sukkur', 'Lahore'];
+console.log(arrayCities);
+
+// add new city name in array in last
+arrayCities.push('Quetta')
+console.log(arrayCities)
+
+// add some country names in blank array 
+
+let arrayCountry: string [] = [];
+
+arrayCountry.push('Germany');
+arrayCountry.push('Italy');
+arrayCountry.push('Spain');
+
+console.log(arrayCountry);
+
