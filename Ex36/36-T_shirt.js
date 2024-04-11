19029557e007f5981792e8e114c7bdb8da775cb4
nadeem-ahmed-77 <165781212+nadeem-ahmed-77@users.noreply.github.com>
2024-04-11 21:28:34 +0500

    Add files via upload

---
+++ Ex36/36-T_shirt.js
@@ -0,0 +1,8 @@
+"use strict";
+// 36. T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. The function should print a sentence summarizing the size of the shirt and the message printed on it.Call the function.
+function tShirt(size, text) {
+    console.log(`printing  a ${size} T-shirt with the message ${text}:`);
+}
+tShirt('large', 'Absolutely Not');
+tShirt('medium', 'Absolutely Not');
+tShirt('small', 'Absolutely Not');
---
+++ Ex36/36-T_shirt.ts
@@ -0,0 +1,15 @@
+
+// 36. T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. The function should print a sentence summarizing the size of the shirt and the message printed on it.Call the function.
+
+
+
+function tShirt(size:string, text:string){
+
+    console.log(`printing  a ${size} T-shirt with the message ${text}:`);
+}
+ 
+tShirt('large', 'Absolutely Not');
+tShirt('medium', 'Absolutely Not');
+tShirt('small', 'Absolutely Not');
+
+    
