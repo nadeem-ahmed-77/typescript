a175564f41db5d943fb63710a6d9a7a61af6f536
nadeem-ahmed-77 <165781212+nadeem-ahmed-77@users.noreply.github.com>
2024-04-11 20:31:57 +0500

    Add files via upload

---
+++ Ex12/11-names.ts
@@ -0,0 +1,11 @@
+// 11. Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+
+
+
+let friendNames :string[] = ["Nadeem", "Zubair", "Hassan", "Iqbal"];
+
+for (let i=0; i<friendNames.length; i++){
+ 
+    console.log(friendNames[i]);   
+
+}
