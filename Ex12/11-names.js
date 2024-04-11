54804e714990c5b944cdf26c876498c3f37d9cc4
nadeem-ahmed-77 <165781212+nadeem-ahmed-77@users.noreply.github.com>
2024-04-11 20:34:37 +0500

    Delete Ex12/11-names.js

--- Ex12/11-names.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// 11. Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
-let friendNames = ["Nadeem", "Zubair", "Hassan", "Iqbal"];
-for (let i = 0; i < friendNames.length; i++) {
-    console.log(friendNames[i]);
-}
