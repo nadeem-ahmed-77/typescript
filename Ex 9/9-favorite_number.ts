476b9b9fb8366f8003daa85ad362562f86906cd5
nadeem-ahmed-77 <165781212+nadeem-ahmed-77@users.noreply.github.com>
2024-04-11 20:26:41 +0500

    Add files via upload

---
+++ Ex 9/9-favorite_number.ts
@@ -0,0 +1,9 @@
+
+// 9. Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+
+const favoriteNumber: number = 6
+
+const message: string = "Here is my favorite number " + favoriteNumber ;
+
+console.log(message);
